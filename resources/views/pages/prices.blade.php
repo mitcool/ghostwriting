d6a604645fadb1ec20496ab889c7b2806b13b14f
viktor <viktor.peev@onsites.com>
2022-03-24 11:01:39 +0200

    prices calculation js function

--- resources/views/pages/prices.blade.php
+++ resources/views/pages/prices.blade.php
@@ -19,29 +19,29 @@
 							</div>
 						</div>
 						<div>
-							<select class="form-control my-2">
-								<option>Select main category</option>
-								<option>Category 1</option>
-								<option>Category 2</option>
-								<option>Category 3</option>
+							<select id="num1" class="form-control my-2">
+								<option value="0">Select main category</option>
+								<option value="10">Category 1</option>
+								<option value="20">Category 2</option>
+								<option value="30">Category 3</option>
 							</select>
-							<select class="form-control my-2">
-								<option>Select subject area</option>
-								<option>Subject 1</option>
-								<option>Subject 2</option>
-								<option>Subject 3</option>
+							<select id="num2" class="form-control my-2">
+								<option value="0">Select subject area</option>
+								<option value="10">Subject 1</option>
+								<option value="20">Subject 2</option>
+								<option value="30">Subject 3</option>
 							</select>
-							<select class="form-control my-2">
-								<option>Select sub-category</option>
-								<option>Subcategory 1</option>
-								<option>Subcategory 2</option>
-								<option>Subcategory 3</option>
+							<select id="num3" class="form-control my-2">
+								<option value="0">Select sub-category</option>
+								<option value="10">Subcategory 1</option>
+								<option value="20">Subcategory 2</option>
+								<option value="30">Subcategory 3</option>
 							</select>
-							<select class="form-control my-2">
-								<option>Select language</option>
-								<option>Language 1</option>
-								<option>Language 2</option>
-								<option>Language 3</option>
+							<select id="num4" class="form-control my-2">
+								<option value="0">Select language</option>
+								<option value="10">Language 1</option>
+								<option value="20">Language 2</option>
+								<option value="30">Language 3</option>
 							</select>
 							<hr>
 							<h5 class="d-flex fw-bold justify-content-between">
@@ -73,5 +73,19 @@
 			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+		function calculate() {
+	 	var num1 = +document.getElementById("num1").value;
+	 	var num2 = +document.getElementById("num2").value;
+	  	var num3 = +document.getElementById("num3").value;
+	  	var num4 = +document.getElementById("num4").value;
+	  document.getElementById("price").innerHTML = eval(num1 + num2 + num3 + num4);
+	}
+	num1.onchange=calculate;
+	num2.onchange=calculate;
+	num3.onchange=calculate;
+	num4.onchange=calculate;
+	calculate();
+	</script>
 	
 @endsection
